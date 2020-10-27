662c4008fe03dc71a00073cef69f3888f222cc63
Mars Wong <huangruoxing@bytedance.com>
2020-10-27 19:33:25 +0800

    feat(web_console): support update user name

--- web_console/pages/setting.jsx
+++ web_console/pages/setting.jsx
@@ -1,21 +1,27 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 import useSWR from 'swr';
-import { Button, Divider, Fieldset, Link, Note, Select, Tag, Text, useTheme } from '@zeit-ui/react';
+import { Button, Divider, Fieldset, Input, Link, Note, Select, Tag, Text, useTheme, useInput } from '@zeit-ui/react';
 import { fetcher } from '../libs/http';
 import Layout from '../components/Layout';
+import { updateMe } from '../services';
 import { updateDeployment } from '../services/deployment';
 
 export default function Setting() {
   const theme = useTheme();
+  const { state: name, setState: setName, bindings: nameBindings } = useInput('');
+  const { data: userData } = useSWR('user', fetcher);
+  const user = userData?.data ?? null;
   const { data: deploymentData } = useSWR('deployments/fedlearner-web-console', fetcher);
   const deployment = deploymentData?.data ?? null;
   const { data: activityData } = useSWR('activities', fetcher);
-  const activities = activityData?.data.filter((x) => x.type === 'release') ?? [];
+  const activities = activityData?.data?.filter((x) => x.type === 'release') ?? [];
   const version = useMemo(() => deployment?.spec.template.spec.containers[0].image.split(':')[1], [deployment]);
   const versionLink = useMemo(() => `https://github.com/marswong/fedlearner/releases/tag/${version}`, [version]);
   const [customVersion, setCustomVersion] = useState('');
   const [upgrading, setUpgrading] = useState(false);
+  const [updatingUser, setUpdatingUser] = useState(false);
   const [error, setError] = useState(null);
+  const [nameError, setNameError] = useState(null);
 
   const handleUpgrade = async () => {
     setUpgrading(true);
@@ -34,6 +40,31 @@ export default function Setting() {
     setUpgrading(false);
   };
 
+  const handleUpdateUser = async () => {
+    setUpdatingUser(true);
+
+    try {
+      const res = await updateMe({
+        ...user,
+        name,
+      });
+
+      if (res.error) {
+        throw new Error(res.error);
+      }
+    } catch (err) {
+      setNameError(err.message);
+    }
+
+    setUpdatingUser(false);
+  };
+
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+    }
+  }, [user]);
+
   return (
     <Layout>
       <Text h2>Settings</Text>
@@ -41,6 +72,7 @@ export default function Setting() {
 
       <Divider />
 
+      {/* Version */}
       <Fieldset>
         <Fieldset.Title>
           Web Console Version
@@ -69,6 +101,31 @@ export default function Setting() {
           <Fieldset.Footer.Actions>
             <Button auto disabled={!customVersion} loading={upgrading} type="secondary" onClick={handleUpgrade}>
               Upgrade
+            </Button>
+          </Fieldset.Footer.Actions>
+        </Fieldset.Footer>
+      </Fieldset>
+
+      {/* Name */}
+      <Fieldset style={{ marginTop: theme.layout.gap }}>
+        <Fieldset.Title>
+          Your Name
+        </Fieldset.Title>
+        <Fieldset.Subtitle>
+          Please enter your full name, or a display name you are comfortable with.
+        </Fieldset.Subtitle>
+
+        {user && <Input {...nameBindings} />}
+
+        <Fieldset.Footer>
+          <Fieldset.Footer.Status>
+            {nameError
+              ? <Note small label="error" type="error">{nameError}</Note>
+              : <Text p>Please use 200 characters at maximum.</Text>}
+          </Fieldset.Footer.Status>
+          <Fieldset.Footer.Actions>
+            <Button auto loading={updatingUser} type="secondary" onClick={handleUpdateUser}>
+              Save
             </Button>
           </Fieldset.Footer.Actions>
         </Fieldset.Footer>
